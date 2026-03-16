2f4ecd528a063c0a70693a2def530249788de3ed
Daksh256 <Dakshjaiswal.four@gmail.com>
2026-03-16 14:37:28 +0530

    updated login page to use spotify color scheme

--- app/src/main/java/com/example/spotifystats/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/spotifystats/ui/login/LoginScreen.kt
@@ -1,9 +1,11 @@
 package com.example.spotifystats.ui.login
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,32 +21,72 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.spotifystats.R
 import com.example.spotifystats.ui.theme.SpotifyStatsTheme
 @Composable
 fun LoginScreen(){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize().size(100.dp).background(MaterialTheme.colorScheme.background)
+        modifier = Modifier
+            .fillMaxSize()
+            .size(100.dp)
+            .background(MaterialTheme.colorScheme.background)
     ) {
+
+        Spacer(Modifier.weight(1f))
+
+        Text(
+            text = "Welcome!",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Connect to see your top tracks, artists, and listening history.",
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(Modifier.weight(1f))
 
         Button(
             onClick = {},
             shape = RoundedCornerShape(16.dp),
-            modifier = Modifier.fillMaxWidth().height(100.dp).padding(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(16.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.primary,
                 contentColor = MaterialTheme.colorScheme.onPrimary
             )
         ) {
+            Image(
+                painter = painterResource(R.drawable.spotify_music_download_playlist_streaming_media_black_and_white_812877a098e0e84df9c8e20c435e581b),
+                contentDescription = null
+            )
+
             Text(
                 text = "Connect With Spotify",
-                fontSize = 23.sp
+                fontSize = 23.sp,
+                fontWeight = FontWeight.Bold
             )
         }
+
+        Spacer(Modifier.weight(0.5f))
     }
 }
 
--- app/src/main/java/com/example/spotifystats/ui/theme/Color.kt
+++ app/src/main/java/com/example/spotifystats/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Shego = Color(0xFF11FF00)
+val Shego = Color(0xFF1ED760)
 
 val Black = Color(0xFF000000)
 
