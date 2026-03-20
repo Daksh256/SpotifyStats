5c0af79c2cd3b575e43c4fe0bff6860f8e920eec
Daksh256 <Dakshjaiswal.four@gmail.com>
2026-03-21 02:33:28 +0530

    Implementing and Testing Logout button

--- app/src/main/java/com/example/spotifystats/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/spotifystats/ui/home/HomeScreen.kt
@@ -1,14 +1,24 @@
 package com.example.spotifystats.ui.home
 
+import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.spotifystats.R
 import com.example.spotifystats.ui.theme.SpotifyStatsTheme
 
 
@@ -16,6 +26,8 @@ import com.example.spotifystats.ui.theme.SpotifyStatsTheme
 fun HomeScreen(
     navController: NavController
 ){
+
+    val context = LocalContext.current
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -24,6 +36,26 @@ fun HomeScreen(
         Text(
             text = "This is the HomeScreen"
         )
+
+        Button(
+            onClick = {
+                val sharedPreferences = context.getSharedPreferences("SpotifyStatsPrefs", Context.MODE_PRIVATE)
+
+                sharedPreferences.edit().remove("AUTH_CODE").apply()
+
+                navController.navigate("LoginScreen") {
+                    popUpTo(0) { inclusive = true }
+                }
+            }
+        ) {
+            Text(
+                text = "Logout"
+            )
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Logout your account"
+            )
+        }
     }
 }
 
